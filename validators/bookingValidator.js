909871e4b7be106efb9e9c940638d0ab125d0126
mohannad <mohannadalshoushi@gmail.com>
2024-12-29 14:15:19 +0300

    add captilze the firstChar to the first name

--- validators/bookingValidator.js
+++ validators/bookingValidator.js
@@ -7,11 +7,12 @@ module.exports.validateBooking = (req, res, next) => {
     "Ocean View",
     "Amman Grand Hotel",
   ];
-
   if (!firstName || firstName.length < 3)
     return res
       .status(400)
       .json({ error: "First name must be at least 3 letters." });
+  const formattedFirstName =
+    firstName.charAt(0).toUpperCase() + firstName.slice(1);
 
   if (!lastName) {
     return res.status(400).json({ error: "please enter your last name" });
@@ -34,5 +35,6 @@ module.exports.validateBooking = (req, res, next) => {
   if (!validHotels.includes(hotelName))
     return res.status(400).json({ error: "Hotel name must be valid." });
 
+  req.body.firstName = formattedFirstName;
   next();
 };
